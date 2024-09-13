4a2f67e5cd997146f3fd2f97a0f9479dbec19d49
icey <icey@icey.tech>
2024-09-13 08:46:29 +0800

    commandline functionality

--- main.c
+++ main.c
@@ -2,21 +2,27 @@
 
 #include <string.h>
 
-int main() {
-    r11f_classfile_t classfile;
-    memset(&classfile, 0, sizeof(r11f_classfile_t));
+int main(int argc, char *argv[]) {
+    if (argc >= 3 && !strcmp(argv[1], "--dump")) {
+        for (int i = 2; i < argc; i++) {
+            r11f_classfile_t classfile;
+            memset(&classfile, 0, sizeof(r11f_classfile_t));
 
-    FILE *fp = fopen("Main.class", "rb");
-    if (!fp) {
-        fprintf(stderr, "failed to open file\n");
-        return 1;
+            FILE *fp = fopen(argv[i], "rb");
+            if (!fp) {
+                fprintf(stderr, "failed to open file\n");
+                continue;
+            }
+
+            if (!r11f_classfile_read(fp, &classfile)) {
+                fprintf(stderr, "failed to read classfile\n");
+                r11f_classfile_cleanup(&classfile);
+                fclose(fp);
+            }
+
+            r11f_classfile_dump(argv[i], &classfile);
+            r11f_classfile_cleanup(&classfile);
+            fclose(fp);
+        }
     }
-
-    if (!r11f_classfile_read(fp, &classfile)) {
-        fprintf(stderr, "failed to read classfile\n");
-        return 1;
-    }
-
-    r11f_classfile_dump("Main.class", &classfile);
-    r11f_classfile_cleanup(&classfile);
 }
